774580d71959accb053114c63e6698817268cb4c
Stephen Chang <stchang@ccs.neu.edu>
2017-10-11 16:30:16 -0400

    delete/modify files for rcon17 tutorial

--- info.rkt
+++
@@ -1,9 +0,0 @@
-#lang setup/infotab
-
-(define version "0.2")
-(define collection "bestfit")
-(define name "bestfit")
-(define deps '("base" "typed-racket-lib" "plot-lib" "plot-gui-lib" "math-lib"))
-(define build-deps '("racket-doc" "typed-racket-doc" "scribble-lib" "math-doc" "plot-doc"))
-
-(define scribblings '(("scribblings/bestfit.scrbl" ())))
